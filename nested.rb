a52a5e18bd1eee7f71d036084a60b40140f75c3b
generous-vector-9581 <ojappah@gmail.com>
2019-09-26 23:45:28 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -4,6 +4,7 @@ def hopper
  		{
         :grace_hopper => {
           :known_for => "COBOL",
+          :languages => ["COBOL", "FORTRAN"]
         },
         :alan_kay => {
           :known_for => "Object Orientation",
